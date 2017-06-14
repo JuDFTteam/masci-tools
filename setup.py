9a1827bd3c9f6860f0a0a6e6637ce7d62de526d3
J.Broeder <j.broeder@fz-juelich.de>
2017-06-14 15:33:12 +0200

    made plotmethods pip installable

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+setup: usage: pip install -e .[graphs]
+"""
+
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(
+        name='plot_methods',
+        version='0.1.0b',
+        description='Wrapers of matplotlib functionality to visualalize common material science data. Plus wrapers of visualisation for aiida-fleur workflow nodes',
+        url='https://bitbucket.org/broeder-j/plot_methods/',
+        author='Jens Broeder',
+        author_email='j.broeder@fz-juelich.de',
+        license='MIT License, see LICENSE.txt file.',
+        classifiers=[
+            'Development Status :: 2 - Beta',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Scientific/Engineering :: Physics'
+        ],
+        keywords='material science plots fitting visualization aiida aiida-fleur flapw',
+        packages=find_packages(exclude=['aiida']),
+        include_package_data=True,
+        install_requires=[
+            'numpy',
+            'matplotlib',
+        ],
+    )
