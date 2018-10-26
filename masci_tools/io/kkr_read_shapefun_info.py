8c2c22b967e84a99ac20231e4657fcb229e2c544
Philipp Rüssmann <p.ruessmann@fz-juelich.de>
2018-10-26 10:04:41 +0200

    Corrected shapfun plotting tool

--- masci_tools/io/kkr_read_shapefun_info.py
+++ masci_tools/io/kkr_read_shapefun_info.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
-# Copyright (c), 2018 Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany.         #
+# Copyright (c), 2018 Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany.    #
 #                All rights reserved.                                         #
 # This file is part of the Masci-tools package.                               #
 # (Material science tools)                                                    #
@@ -10,60 +10,75 @@
 #                                                                             #
 ###############################################################################
 
-"""
-:author: Philipp Ruessmann
-
-Plotting utility to visualize the output of the voronoi code.
-
-Reads files 'vertices.dat' to extract the vertices of the shapefunctions and 'positions.dat' (done in 'read_shapefun' function)
-
-Then creates a simple matplotlib image to show the shapefunctions using the 'plot_shapefun' function.
-
-"""
-
 
 def read_shapefun(path='.'):
-   """
-   Read vertices of shapefunctions with 
-   Zoom into shapefun of a single atom
+  """
+  Read vertices of shapefunctions with 
+  Zoom into shapefun of a single atom
 
-   :author: Philipp Ruessmann
+  :author: Philipp Ruessmann
 
-   :param path: path where voronoi output is found (optional, defaults to './')
+  :param path: path where voronoi output is found (optional, defaults to './')
 
-   :returns pos: positions of the centers of the shapefunctions
-   :returns out: dictionary of the vertices of the shapefunctions
-   """
-   path += '/'
-   f = open(path+'vertices.dat')
+  :returns pos: positions of the centers of the shapefunctions
+  :returns out: dictionary of the vertices of the shapefunctions
+  """
+  from common_functions import search_string
+  from numpy import array
 
-   out = {}
-   i_line = 1
-   face, face_old = -1,-1
-   for line in f:
-	if 'representative' in line:
-		atm = int(line.split()[-1])
-		i_newat = 1
-	elif 'Face' in line:
-		i_newat = 0
-		face = int(line.split()[-1])
-		i_newface = 1
-		tmp = []
-	else:
-		i_newface = 0
-		tmp2 = line.split()
-		if tmp2<>[]:
-			tmp2 = [float(i) for i in tmp2]
-			tmp.append(tmp2)
-		atm_old, face_old = atm,face
-		print i_line,atm,face
-	if face_old<>face:
-		out[atm,face] = tmp
-	i_line += 1
+  path += '/'
 
-   f.close()
+  # first read vertices file
+  with open(path+'vertices.dat') as f:
+    out = {}
+    face, face_old = -1,-1
+    for line in f:
+      if '# serial:' in line[:9]:
+        # skip line with serial number
+        pass
+      else:
+        if 'representative' in line:
+          atm = int(line.split()[-1])
+          i_newat = 1
+        elif 'Face' in line:
+          i_newat = 0
+          face = int(line.split()[-1])
+          i_newface = 1
+          tmp = []
+        else:
+          i_newface = 0
+          tmp2 = line.split()
+          if tmp2<>[]:
+            tmp2 = [float(i) for i in tmp2]
+            tmp.append(tmp2)
+          atm_old, face_old = atm,face
+        if face_old<>face:
+          out[atm,face] = tmp
 
-   pos = loadtxt('positions.dat')
+  # then read positions from inputcard
+  with open('inputcard') as file:
+    inp = file.readlines()
+    # convert to uppercase
+    for iline in range(len(inp)):
+      inp[iline] = inp[iline].upper()
+    # search number of atoms in inputcard
+    iline = search_string('NAEZ', inp)
+    if iline<0:
+      iline = search_string('NATYP', inp)
+      if iline==-1:
+        raise ValueError("Error reading NAEZ or NATYP from inputcard.")
+    naez = int(inp.pop(iline).split('=')[1].split()[0])
 
-   return pos, out
+    # read rbasis positions
+    iline = search_string('RBASIS', inp)
+    if iline>=0:
+      pos = []
+      for iatom in range(naez):
+        pos.append(inp.pop(iline+1))
+      # finally convert to float arrays
+      pos = array([[float(j) for j in i.split()[:3]] for i in pos])
+    else:
+      raise ValueError("Error reading RBASIS from inputcard.")
+
+  return pos, out
 
--- masci_tools/vis/kkr_plot_shapefun.py
+++ masci_tools/vis/kkr_plot_shapefun.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
-# Copyright (c), 2018 Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany.         #
+# Copyright (c), 2018 Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany.    #
 #                All rights reserved.                                         #
 # This file is part of the Masci-tools package.                               #
 # (Material science tools)                                                    #
@@ -11,78 +11,81 @@
 ###############################################################################
 
 
-
 def plot_shapefun(pos, out, mode):
-   """
-   Creates a simple matplotlib image to show the shapefunctions given it's positions in the unit cell, the atoms's vertices in `ut` and the plotting mode
+  """
+  Creates a simple matplotlib image to show the shapefunctions given it's positions in the unit cell, the atoms's vertices in `ut` and the plotting mode
 
-   :author: Philipp Ruessmann
+  :author: Philipp Ruessmann
 
-   :param pos: positions of the centers of the cells
-   :param verts: array of vertices of the shapefunction (outlines of shapes)
-   :param mode: 'all' or 'single' determines whether or not all shapes are combined in a single figure or plotted as individual figures
+  :param pos: positions of the centers of the cells
+  :param verts: array of vertices of the shapefunction (outlines of shapes)
+  :param mode: 'all' or 'single' determines whether or not all shapes are combined in a single figure or plotted as individual figures
 
-   :returns ax: return the axis in which the plot was done (useful to pass to 'change_zoom' and 'zoom_in' functions of this module
-   """
-   import matplotlib.pyplot as plt
-   from mpl_toolkits.mplot3d import Axes3D
+  :returns ax: return the axis in which the plot was done (useful to pass to 'change_zoom' and 'zoom_in' functions of this module
+  """
+  import matplotlib.pyplot as plt
+  from mpl_toolkits.mplot3d import Axes3D
+  from numpy import array, shape
 
-   if mode=='all':
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection='3d')
+  if mode=='all':
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-   Natm = len(pos)
+  Natm = len(pos)
 
-   for j in range(1,Natm+1):
-	if mode=='single':
-        	fig = plt.figure()
-        	ax = fig.add_subplot(111, projection='3d')
-	for i in out.keys():
-	    if i[0]==j:
-		linecolor = 'r'
-		if j in [2,3,6,7]:
-			linecolor='b'
-	        d = array(out[i])
-		dx,dy,dz = pos[j-1,0],pos[j-1,1],pos[j-1,2] 
-	        #plot3d(d[:,0]+dx,d[:,1]+dy,d[:,2]+dz)
-		ax.plot(d[:,0]+dx,d[:,1]+dy,d[:,2]+dz,linecolor)
-		#title('Atom '+str(j))
-   if mode=='all':
-        xy_scale = 1.3
-        scale = 15
-	ax.set_xlim(-scale*xy_scale,scale*xy_scale)
-	ax.set_ylim(-scale*xy_scale,scale*xy_scale)
-	ax.set_zlim(-scale,scale)
+  for j in range(1,Natm+1):
+    if mode=='single':
+      fig = plt.figure()
+      ax = fig.add_subplot(111, projection='3d')
+    for i in out.keys():
+      if i[0]==j:
+        linecolor = 'r'
+      if j in [2,3,6,7]:
+        linecolor='b'
+      d = array(out[i])
+      if len(shape(pos))>1 and len(pos)>2 and mode=='all':
+        dx, dy, dz = pos[j-1,0], pos[j-1,1], pos[j-1,2] 
+      else:
+        dx, dy, dz = 0, 0, 0
+      ax.plot(d[:,0]+dx,d[:,1]+dy,d[:,2]+dz,linecolor)
 
-   return ax
+  if mode=='all':
+    xy_scale = 1
+    scale = 1
+    ax.set_xlim(-scale*xy_scale,scale*xy_scale)
+    ax.set_ylim(-scale*xy_scale,scale*xy_scale)
+    ax.set_zlim(-scale,scale)
+
+  return ax
 
 
 def change_zoom(ax, zoom_range, center=[0,0,0]):
-     """
-     Change the zoom of a 3d plot
+  """
+  Change the zoom of a 3d plot
 
-     :author: Philipp Ruessmann
+  :author: Philipp Ruessmann
 
-     :param ax: axis which is zoomed
-     :param zoom_range: range to which the image is zoomed, total range from center-zoom_range to center+zoom_range
-     :param center: center of the zoomed region (optional, defaults to origin)
-     """
-     ax.set_xlim(center[0]-zoom_range,center[0]+zoom_range)
-     ax.set_ylim(center[1]-zoom_range,center[1]+zoom_range)
-     ax.set_zlim(center[2]-zoom_range,center[2]+zoom_range)
+  :param ax: axis which is zoomed
+  :param zoom_range: range to which the image is zoomed, total range from center-zoom_range to center+zoom_range
+  :param center: center of the zoomed region (optional, defaults to origin)
+  """
+  ax.set_xlim(center[0]-zoom_range,center[0]+zoom_range)
+  ax.set_ylim(center[1]-zoom_range,center[1]+zoom_range)
+  ax.set_zlim(center[2]-zoom_range,center[2]+zoom_range)
+
 
 def zoom_in(ax, atm, pos, zoom_range=10):
-     """
-     Zoom into shapefun of a single atom
+  """
+  Zoom into shapefun of a single atom
 
-     :author: Philipp Ruessmann
+  :author: Philipp Ruessmann
 
-     :param ax: axis in which shapefun plot is found
-     :param atm: atom index whose shapefunction is zoomed
-     :param pos: array of positions of centers of the shapes (needed to shift center of zommed region to correct atom
-     :param zoom_range: range of the zoomed region (optional, defaults to 10)
-     """
-     center = pos[atm-1]
-     print 'zoom in to atom', atm, ':', center
-     change_zoom(ax, zoom_range, center)
+  :param ax: axis in which shapefun plot is found
+  :param atm: atom index whose shapefunction is zoomed
+  :param pos: array of positions of centers of the shapes (needed to shift center of zommed region to correct atom
+  :param zoom_range: range of the zoomed region (optional, defaults to 10)
+  """
+  center = pos[atm-1]
+  print 'zoom in to atom', atm, ':', center
+  change_zoom(ax, zoom_range, center)
 
