f96630d55b32cec1c13f42a6c57bd0e7a4d45e7b
Christian Partmann <christian@partmann.com>
2018-12-14 19:14:31 +0100

    new colorbar

--- SiSc_Lab/new_reader.py
+++ SiSc_Lab/new_reader.py
@@ -36,8 +36,9 @@ atom_group = f["/atoms/equivAtomsGroup"]
 
 (NUM_SPIN, NUM_K, NUM_E, NUM_GROUPS, NUM_CHAR) = llc.shape
 
-# ugly, but doesn't matter for total runtime
-# check if rec_cell or rec_cell.T is correct!!!!!!!!!!!
+"""
+convert from internal to physical units
+"""
 def k_phys(f):
     kpts_int = f["/kpts/coordinates"]
     rec_cell = f["/cell/reciprocalCell"][:]
@@ -52,6 +53,9 @@ def k_phys(f):
         k_dist[i] = k_dist[i-1] + np.sqrt((kx[i]-kx[i-1])**2 +(ky[i]-ky[i-1])**2 +(kz[i]-kz[i-1])**2)
     return k_dist
 
+"""
+create filters to select the required Spins, Groups, Bands, Characters
+"""
 def create_spin_filter(which_spin, NUM_SPIN = NUM_SPIN):
     SPIN_FILTER = np.zeros(NUM_SPIN).astype(bool)
     SPIN_FILTER[which_spin] = True
@@ -85,9 +89,11 @@ def get_k_special_pt(ind_special_points, k):
 
 k_special_pt = get_k_special_pt(special_points, k)
 
-# this is the expensive funkction!
-# reads all data, selects relevant parts and reduces along groups and characters
-# generic form: pass lists of booleans...
+"""
+processes the data to obtain the weights:
+this is the function with most significant runtime!
+
+"""
 def get_data(f, llc, SPIN_FILTER, CHARACTER_FILTER, GROUP_FILTER, BAND_FILTER, UNFOLD_WEIGHT=band_unfolding, unfoldong_weight_exponent = 1):
     ATOMS_PER_GROUP = np.zeros(NUM_GROUPS)
     for i in range(NUM_GROUPS):
@@ -120,6 +126,10 @@ def get_data(f, llc, SPIN_FILTER, CHARACTER_FILTER, GROUP_FILTER, BAND_FILTER, U
     
     return llc_normalized
 
+"""
+reshapes the 2 dimensional field of weights into a 1d array to speed up plotting
+--> avoids to call the scatter plot for every band
+"""
 def reshape_data(f, llc, evs, k, spin, CHARACTER_FILTER, GROUP_FILTER, BAND_FILTER, UNFOLD_WEIGHT=band_unfolding, unfoldong_weight_exponent = 1):
     SPIN_FILTER = create_spin_filter(spin)
     total_weight = get_data(f, llc, SPIN_FILTER, CHARACTER_FILTER, GROUP_FILTER, BAND_FILTER, UNFOLD_WEIGHT, unfoldong_weight_exponent)
@@ -138,34 +148,6 @@ label = []
 for i in range(len(special_points_label)):
     label += str(special_points_label[i])[2]
 
-times += [time.time()]
-
-spin = 0
-unfoldong_weight_exponent = 1.0
-SPIN_FILTER = create_spin_filter(spin)
-CHARACTER_FILTER = create_character_filter([0,1,2,3])
-GROUP_FILTER = create_group_filter()
-UNFOLD_WEIGHT = band_unfolding
-BAND_FILTER = create_band_filter()
-
-"""
-(k_r, E_r, W_r) = reshape_data(f, llc, evs, k, spin, CHARACTER_FILTER, GROUP_FILTER, BAND_FILTER, UNFOLD_WEIGHT, unfoldong_weight_exponent)
-times += [time.time()]
-
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-
-#just plot points with minimal size of t
-speed_up = True
-if(speed_up == True):
-    t = 1e-4
-    k_r = k_r[W_r>t]
-    E_r = E_r[W_r>t]
-    W_r = W_r[W_r>t]
-
-ax1.scatter(k_r, (E_r-fermi_energy)*hartree_in_ev, marker='o', c='b', s = 4 * W_r, lw=0)
-"""
-
 def plot(color, ax1, f, llc, evs, k, spin, CHARACTER_FILTER, GROUP_FILTER, BAND_FILTER, UNFOLD_WEIGHT, unfoldong_weight_exponent, alpha = 1):
     (k_r, E_r, W_r) = reshape_data(f, llc, evs, k, spin, CHARACTER_FILTER, GROUP_FILTER, BAND_FILTER, UNFOLD_WEIGHT, unfoldong_weight_exponent)
     #just plot points with minimal size of t
@@ -179,7 +161,25 @@ def plot(color, ax1, f, llc, evs, k, spin, CHARACTER_FILTER, GROUP_FILTER, BAND_
     #print(max(W_r))
     return 0
 
-"""
+def configure_plot(filename = False):
+    plt.xticks(k_special_pt, label)
+    plt.ylabel("E(k) [eV]")
+    plt.xlim(0, max(k))
+    plt.hlines(0, 0, max(k), lw = 0.1)
+    #if(isinstance(filename, str)):
+        #plt.savefig(filename+str(".png"), dpi=1000)
+    return 0
+        
+times += [time.time()]
+
+spin = 0
+unfoldong_weight_exponent = 1.0
+SPIN_FILTER = create_spin_filter(spin)
+CHARACTER_FILTER = create_character_filter([0,1,2,3])
+GROUP_FILTER = create_group_filter()
+UNFOLD_WEIGHT = band_unfolding
+BAND_FILTER = create_band_filter()
+
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 alpha = 0.5
@@ -191,14 +191,7 @@ plot("green", ax1, f, llc, evs, k, 0, create_character_filter([2]), create_group
      band_unfolding, 1, alpha)
 plot("yellow", ax1, f, llc, evs, k, 0, create_character_filter([3]), create_group_filter(), create_band_filter(),
      band_unfolding, 1, alpha)
-
-plt.xticks(k_special_pt, label)
-plt.ylabel("E(k) [eV]")
-plt.xlim(0, max(k))
-plt.hlines(0, 0, max(k), lw = 0.1)
-#plt.savefig("dos_characters.png", dpi=2000)
-"""
-
+configure_plot("all_chars")
 
 """
 # for dos_file
@@ -210,11 +203,7 @@ plot("red", ax2, f, llc, evs, k, 0, create_character_filter([0,1,2,3]), create_g
      band_unfolding, 1, alpha)
 plot("green", ax2, f, llc, evs, k, 0, create_character_filter([0,1,2,3]), create_group_filter([3]), create_band_filter(),
      band_unfolding, 1, alpha)
-plt.xticks(k_special_pt, label)
-plt.ylabel("E(k) [eV]")
-plt.xlim(0, max(k))
-plt.hlines(0, 0, max(k), lw = 0.1)
-plt.savefig("dos_groups_012_4_3.png", dpi=2000)
+configure_plot()
 """
 
 def plot_two_characters(color, ax1, f, llc, evs, k, spin, CHARACTER_FILTER, GROUP_FILTER, BAND_FILTER, UNFOLD_WEIGHT, 
@@ -226,92 +215,34 @@ def plot_two_characters(color, ax1, f, llc, evs, k, spin, CHARACTER_FILTER, GROU
         
     (k_resh, evs_resh, weight_resh) = reshape_data(f, llc, evs, k, spin, create_character_filter([characters[0]]), GROUP_FILTER, BAND_FILTER, UNFOLD_WEIGHT=band_unfolding, unfoldong_weight_exponent = 1)
     (k_resh2, evs_resh2, weight_resh2) = reshape_data(f, llc, evs, k, spin, create_character_filter([characters[1]]), GROUP_FILTER, BAND_FILTER, UNFOLD_WEIGHT=band_unfolding, unfoldong_weight_exponent = 1)
-    #print(k_resh-k_resh2)
-    #print(evs_resh-evs_resh2)
+
     rel = weight_resh/(weight_resh+weight_resh2)*20
     #ax1.scatter(k_resh, (evs_resh-fermi_energy)*hartree_in_ev, marker='o', c="g", s = 5 * weight_resh, lw=0, alpha = alpha)
     #ax1.scatter(k_resh2, (evs_resh-fermi_energy)*hartree_in_ev, marker='o', c="r", s = 5 * weight_resh2, lw=0, alpha = alpha)
-    cm = plt.cm.RdBu #get_cmap('RdYlBu')
+    cm = plt.cm.winter #get_cmap('RdYlBu')
     ax1.scatter(k_resh2, (evs_resh-fermi_energy)*hartree_in_ev, marker='o', c=rel, s = 5 * weight_resh2, lw=0, alpha = alpha,cmap=cm)
 
 fig = plt.figure()
 alpha = 1
 ax4 = fig.add_subplot(111)
 plot_two_characters("blue", ax4, f, llc, evs, k, 0, create_character_filter([0,1]), create_group_filter(), create_band_filter(),
-     band_unfolding, 1., alpha)
+     band_unfolding, 0.6, alpha)
+configure_plot("2characters")
 
-plt.savefig("2characters.png", dpi=2000)
 
 fig = plt.figure()
 ax3 = fig.add_subplot(111)
 alpha = 0.2
 plot("blue", ax3, f, llc, evs, k, 0, create_character_filter([0,1,2,3]), create_group_filter(), create_band_filter(),
      band_unfolding, 1., alpha)
-plot("red", ax3, f, llc, evs, k, 0, create_character_filter([0,1,2,3]), create_group_filter(), create_band_filter(range(256, 258)),
-     False, 1., alpha)
-
 """
 #for Co file
 plot("red", ax3, f, llc, evs, k, 1, create_character_filter([0,1,2,3]), create_group_filter(), create_band_filter(),
      band_unfolding, 1, alpha)
 
 """
-
-times += [time.time()]
-plt.xticks(k_special_pt, label)
-plt.ylabel("E(k) [eV]")
-plt.xlim(0, max(k))
-plt.hlines(0, 0, max(k), lw = 0.1)
-#plt.savefig("sphjjhjhin.png", dpi=2000)
-
-
-#plt.xlabel("k")
-
-
-
-#plt.ylim(-10, 5)
-plt.xticks(k_special_pt, label)
-plt.ylabel("E(k) [eV]")
-plt.xlim(0, max(k))
-plt.hlines(0, 0, max(k), lw = 0.1)
-#plt.savefig("testfigure.png", dpi=2000)
+configure_plot()
 
 times += [time.time()]
 times = np.array(times)-times[0]
 print(times)
-
-#%%
-"""
-
-aaa = time.time()
-kk = k_phys(f)
-bbb = time.time()
-kpts_int = f["/kpts/coordinates"]
-rec_cell = f["/cell/reciprocalCell"][:]
-phys_k = np.dot(kpts_int, rec_cell)
-
-shiftp = phys_k[1:, :]
-shiftm = phys_k[:-1, :]
-k_dist = np.append(np.array([0]), np.sum((phys_k[1:, :]-phys_k[:-1, :])**2, axis = 1))
-#for i in range(1, len(k_dist)):
-#    k_dist[i] = k_dist[i-1] + k_dist[i]
-
-ccc = time.time()
-
-print(bbb-aaa)
-print(ccc-bbb)
-"""
-
-"""
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-for n in range(NUM_E):
-        ax2.scatter(k, evs[0, :, n], marker='o', c='b', s = 2 * llc_redGC[0, :, n], lw=0)
-"""  
-
-"""
-SPIN_FILTER = np.array([True])
-CHARACTER_FILTER = np.array([True, False, False, False])
-GROUP_FILTER = np.ones(NUM_GROUPS).astype(bool) #np.array([])
-GROUP_FILTER[1] = False
-"""
