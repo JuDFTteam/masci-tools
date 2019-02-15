2f485edab7becf542191811ad04b4d4371c7ee1d
johannes wasmer <johannes.wasmer@gmail.com>
2019-02-15 16:37:53 +0100

    test: top-level environment.yml for mybinder.org

--- studentproject18w/doc/report/auto/3-implementation.el
+++ studentproject18w/doc/report/auto/3-implementation.el
@@ -1,10 +1,18 @@
 (TeX-add-style-hook
  "3-implementation"
  (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "img/logo/logos")
    (LaTeX-add-labels
     "chap:implementation"
-    "fig:modules"))
+    "fig:modules"
+    "sec:preprocessor"))
  :latex)
 
