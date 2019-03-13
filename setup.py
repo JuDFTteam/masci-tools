c7cf24cc553cf45e18b80dbc6e6990089bb406b7
Philipp Rüssmann <p.ruessmann@fz-juelich.de>
2019-03-13 21:42:57 +0100

    Bump version: 0.3.0-dev → 0.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 if __name__ == '__main__':
     setup(
         name='masci_tools',
-        version='0.3.0-dev',
+        version='0.3.0',
         description='Tools for Materials science. Vis contains wrapers of matplotlib functionality to visualalize common material science data. Plus wrapers of visualisation for aiida-fleur workflow nodes',
         url='https://github.com/JuDFTteam/masci-tools',
         author='Jens Broeder',
