7634d909d6602c9679e653152e928420edf35694
Vasily Tseplyaev <v.tseplyaev@fz-juelich.de>
2019-08-09 08:46:36 +0200

    Fix an encoding issue which appears on cerain OS

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 # read the contents of your README file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
+with open(path.join(this_directory, 'README.md'), encoding="utf8") as f:
     long_description = f.read()
 
 if __name__ == '__main__':
