697b239bd6923babddbd9a4615ccd944cecce769
janssenhenning <henning.janssen@gmx.net>
2020-11-17 14:54:18 +0100

    Add options to make testing of schema_dict generation easier

--- masci_tools/io/parsers/fleur/fleur_schema/inpschema_todict.py
+++ masci_tools/io/parsers/fleur/fleur_schema/inpschema_todict.py
@@ -22,7 +22,7 @@ import importlib.util
 import os
 
 
-def create_inpschema_dict(path):
+def create_inpschema_dict(path, save_to_file=True):
     """
     Creates dictionary with information about the FleurInputSchema.xsd and writes
     it to the same folder in a file called schema_dict.py
@@ -56,14 +56,17 @@ def create_inpschema_dict(path):
     for key, action in schema_actions.items():
         schema_dict[key] = action(xmlschema, namespaces, **schema_dict)
 
-    with open(f'{path}/inpschema_dict.py', 'w') as f:
-        f.write('# -*- coding: utf-8 -*-\n')
-        f.write(f"__inp_version__ = '{inp_version}'\n")
-        f.write('schema_dict = ')
-        pprint(schema_dict, f)
+    if save_to_file:
+        with open(f'{path}/inpschema_dict.py', 'w') as f:
+            f.write('# -*- coding: utf-8 -*-\n')
+            f.write(f"__inp_version__ = '{inp_version}'\n")
+            f.write('schema_dict = ')
+            pprint(schema_dict, f)
+    else:
+        return schema_dict, inp_version
 
 
-def load_inpschema(version, schema_return=False):
+def load_inpschema(version, schema_return=False, create=True):
     """
     load the FleurInputSchema dict for the specified version
     """
@@ -82,8 +85,11 @@ def load_inpschema(version, schema_return=False):
         raise FileNotFoundError(message)
 
     if not os.path.isfile(schema_dict_path):
-        print(f'Generating schema_dict file for given input schema: {schema_file_path}')
-        create_inpschema_dict(path)
+        if create:
+            print(f'Generating schema_dict file for given input schema: {schema_file_path}')
+            create_inpschema_dict(path)
+        else:
+            raise FileNotFoundError(f'No inpschema_dict generated for FleurInputSchema.xsd at {path}')
 
     #import schema_dict
     spec = importlib.util.spec_from_file_location('schema', schema_dict_path)
