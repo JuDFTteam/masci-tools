2208893a3e7a280aaacce74187d7e1567d2be910
Philipp Rüssmann <p.ruessmann@fz-juelich.de>
2020-12-08 21:58:56 +0100

    Fix tests

--- masci_tools/vis/kkr_plot_bandstruc_qdos.py
+++ masci_tools/vis/kkr_plot_bandstruc_qdos.py
@@ -1,48 +1,54 @@
 # -*- coding: utf-8 -*-
+"""
+dispersionplot function for plotting KKR bandstructures (i.e. qdos) files
+"""
+
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-from matplotlib import cm
-from six.moves import range
+from six.moves import range  # pylint: disable=redefined-builtin
 
 
-def dispersionplot(p0='./',
-                   totonly=True,
-                   s=20,
-                   ls_ef=':',
-                   lw_ef=1,
-                   units='eV_rel',
-                   noefline=False,
-                   color='',
-                   reload_data=False,
-                   clrbar=True,
-                   logscale=True,
-                   nosave=False,
-                   atoms=[],
-                   ratios=False,
-                   atoms2=[],
-                   noscale=False,
-                   newfig=False,
-                   cmap=None,
-                   alpha=1.0,
-                   qcomponent=-2,
-                   clims=[],
-                   xscale=1.,
-                   raster=True,
-                   atoms3=[],
-                   alpha_reverse=False,
-                   return_data=False,
-                   xshift=0.,
-                   yshift=0.,
-                   plotmode='pcolor',
-                   ptitle=None,
-                   ef=None,
-                   as_e_dimension=None,
-                   scale_alpha_data=False):
+def dispersionplot(
+    p0='./',
+    totonly=True,
+    s=20,
+    ls_ef=':',
+    lw_ef=1,
+    units='eV_rel',
+    noefline=False,
+    color='',
+    reload_data=False,
+    clrbar=True,
+    logscale=True,
+    nosave=False,
+    atoms=None,
+    ratios=False,
+    atoms2=None,
+    noscale=False,
+    newfig=False,
+    cmap=None,
+    alpha=1.0,
+    qcomponent=-2,
+    clims=None,
+    xscale=1.,
+    raster=True,
+    atoms3=None,
+    alpha_reverse=False,
+    return_data=False,
+    xshift=0.,
+    yshift=0.,
+    plotmode='pcolor',
+    ptitle=None,
+    ef=None,
+    as_e_dimension=None,
+    scale_alpha_data=False,
+    shading='gouraud',
+):
     """ plotting routine for qdos files - dispersion (E vs. q) """
     # import dependencies
-    from numpy import loadtxt, load, save, log, abs, sum, sort, pi, shape, array
-    from matplotlib.pyplot import figure, plot, axvline, scatter, axhline, xlabel, ylabel, title, colorbar, pcolormesh, cm
+    from numpy import loadtxt, load, save, log, abs, sum, sort, pi, shape, array  # pylint: disable=redefined-builtin
+    from matplotlib.pyplot import figure, plot, axvline, scatter, axhline, xlabel, ylabel, title, colorbar, pcolormesh, cm, xlim, ylim, clim
     from os import listdir, getcwd
     from os.path import isdir, getctime
     from time import ctime
@@ -51,11 +57,11 @@ def dispersionplot(p0='./',
     from matplotlib.colors import ListedColormap
 
     # deal with input of file handle instead of path (see plot_kkr of aiida_kkr)
-    if type(p0) != str:
+    if not isinstance(type(p0), str):
         pathname_with_file = p0.name
         p0 = pathname_with_file.replace('/qdos.01.1.dat', '')  #dos.atom1
 
-    if cmap == None:
+    if cmap is None:
         cmap = cm.viridis
     if newfig:
         figure()
@@ -83,7 +89,7 @@ def dispersionplot(p0='./',
         for i in sort(listdir(p0)):
             if 'qdos.' in i[:5] and not isdir(p0 + i):
                 iatom = i.replace('qdos.', '').split('.')[0]
-                if atoms == [] or int(iatom) in atoms:
+                if atoms is None or int(iatom) in atoms:
                     j += 1
                     print(j, p0, i)
                     tmp = loadtxt(p0 + i)
@@ -93,7 +99,7 @@ def dispersionplot(p0='./',
                         first = False
                     else:
                         d[:, 5:] += tmp[:, 5:]
-                if ratios and (atoms2 == [] or int(iatom) in atoms2):
+                if ratios and (atoms2 is None or int(iatom) in atoms2):
                     j += 1
                     print(j, p0, i, 'atoms2=', atoms2)
                     tmp = loadtxt(p0 + i)
@@ -103,7 +109,7 @@ def dispersionplot(p0='./',
                         first2 = False
                     else:
                         d2[:, 5:] += tmp[:, 5:]
-                if (atoms3 == [] or int(iatom) in atoms3) and ratios:
+                if (atoms3 is None or int(iatom) in atoms3) and ratios:
                     j += 1
                     print(j, p0, i, 'atoms3=', atoms3)
                     tmp = loadtxt(p0 + i)
@@ -157,12 +163,12 @@ def dispersionplot(p0='./',
         data = log(data)
     x, y = xscale * sum(d[:, 2:5], axis=1), d[:, 0]
     if qcomponent == -2:
-        el = len(set(d[:, 0]))
+        el = len(set(d[:, 0]))  # pylint disable=unnecessary-comprehension
         if el == 1 and as_e_dimension is not None:
             y = d[:, 2 + as_e_dimension]
             el = len(set(y))
             ylab = r'k (1/Ang.)'
-        x = array([[i for i in range(len(d) // el)] for j in range(el)])
+        x = array([[i for i in range(len(d) // el)] for j in range(el)])  # pylint: disable=unnecessary-comprehension
     elif qcomponent != -1:
         x = xscale * d[:, 2:5][:, qcomponent]
 
@@ -186,7 +192,7 @@ def dispersionplot(p0='./',
         colors[:, -1] = alpha * (dtmp)
         cmap = ListedColormap(colors)
         alpha = 1.
-    if ratios and atoms3 != []:
+    if ratios and atoms3 is not None:
         colors = cmap(data / data.max())
         colors[:, -1] = abs(sum(d3[:, 5:], axis=1)) / abs(sum(d3[:, 5:], axis=1)).max()
         if alpha_reverse:
@@ -197,29 +203,33 @@ def dispersionplot(p0='./',
         else:
             lx = len(set(x.reshape(-1)))
             ly = len(set(y.reshape(-1)))
-            pcolormesh(x.reshape(ly, lx),
-                       y.reshape(ly, lx),
-                       data.reshape(ly, lx),
-                       cmap=cmap,
-                       rasterized=raster,
-                       edgecolor='face',
-                       linewidths=0.0001)
-            #pcolormesh(x.reshape(ly,lx), y.reshape(ly,lx), data.reshape(ly,lx), cmap=cmap, rasterized=raster)
+            pcolormesh(
+                x.reshape(ly, lx),
+                y.reshape(ly, lx),
+                data.reshape(ly, lx),
+                cmap=cmap,
+                rasterized=raster,
+                edgecolor='face',
+                linewidths=0.0001,
+                shading=shading,
+            )
     else:
         if plotmode == 'scatter':
             scatter(x, y, c=data, s=s, lw=0, cmap=cmap, alpha=alpha, rasterized=raster)
         else:
             lx = len(set(x.reshape(-1)))
             ly = len(set(y.reshape(-1)))
-            pcolormesh(x.reshape(ly, lx),
-                       y.reshape(ly, lx),
-                       data.reshape(ly, lx),
-                       cmap=cmap,
-                       rasterized=raster,
-                       edgecolor='face',
-                       linewidths=0.0001)
-            #pcolormesh(x.reshape(ly,lx), y.reshape(ly,lx), data.reshape(ly,lx), cmap=cmap, alpha=alpha, rasterized=raster)
-    if clims != []:
+            pcolormesh(
+                x.reshape(ly, lx),
+                y.reshape(ly, lx),
+                data.reshape(ly, lx),
+                cmap=cmap,
+                rasterized=raster,
+                edgecolor='face',
+                linewidths=0.0001,
+                shading=shading,
+            )
+    if clims is not None:
         clim(clims[0], clims[1])
     if clrbar:
         colorbar()
@@ -234,6 +244,10 @@ def dispersionplot(p0='./',
     # set axis labels
     xlabel(xlab)
     ylabel(ylab)
+
+    # set x and y limits
+    xlim(x.min(), x.max())
+    ylim(y.min(), y.max())
 
     # print path to title
     if totonly and ptitle is None:
