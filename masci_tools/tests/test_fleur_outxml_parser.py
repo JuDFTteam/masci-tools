b372f9df4c2c6262e08c9fe0a6843c3f6e90adbe
janssenhenning <henning.janssen@gmx.net>
2020-12-11 21:37:24 +0100

    Add tests for the outxml_parser (Missing: make sure that the warnings dont contain anything bad)

---
+++ masci_tools/tests/test_fleur_outxml_parser.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+import pytest
+from masci_tools.io.parsers.fleur import outxml_parser
+import os
+
+# Collect the input files
+file_path = 'files/fleur/Max-R5'
+
+
+outxmlfilefolder = os.path.dirname(os.path.abspath(__file__))
+outxmlfilefolder_valid = [
+    os.path.abspath(os.path.join(outxmlfilefolder, file_path))
+]
+
+outxmlfilelist = []
+for folder in outxmlfilefolder_valid:
+    for subdir, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith('.xml') and 'out' in file:
+                 outxmlfilelist.append(os.path.join(subdir, file))
+
+@pytest.mark.parametrize('outxmlfilepath', outxmlfilelist)
+def test_outxml_valid_inpxml(outxmlfilepath):
+    """
+    test if valid inp.xml files are recognized by the inpxml_parser
+    """
+    from lxml import etree
+
+    #Pass inpxmlfile
+    out_dict = outxml_parser(outxmlfilepath)
+
+    #Parse before
+    parser = etree.XMLParser(attribute_defaults=True, encoding='utf-8')
+    xmltree = etree.parse(outxmlfilepath, parser)
+    out_dict = outxml_parser(xmltree)
--- masci_tools/tests/test_schema_dict.py
+++ masci_tools/tests/test_schema_dict.py
@@ -5,6 +5,8 @@ Test of the consistency the input schema dictionaries with the SchemaFiles in th
 import pytest
 import os
 from masci_tools.io.parsers.fleur.fleur_schema import load_inpschema, create_inpschema_dict
+from masci_tools.io.parsers.fleur.fleur_schema import load_outschema, create_outschema_dict
+
 
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,11 +14,16 @@ schema_directory = '../io/parsers/fleur/fleur_schema'
 
 schema_versions = []
 schema_paths = []
+outschema_versions = []
+outschema_paths = []
 for root, dirs, files in os.walk(os.path.abspath(os.path.join(CURRENT_DIRECTORY, schema_directory))):
     for folder in dirs:
         if '0.' in folder:
             schema_versions.append(folder)
             schema_paths.append(os.path.join(root, folder))
+            if int(folder.split('.')[1]) >= 33:
+                outschema_versions.append(folder)
+                outschema_paths.append(os.path.join(root, folder))
 
 
 @pytest.mark.parametrize('schema_version,schema_path', zip(schema_versions, schema_paths))
@@ -26,6 +33,17 @@ def test_inpschema_dict(schema_version, schema_path):
     """
     dict_created, created_version = create_inpschema_dict(schema_path, save_to_file=False)
     dict_stored = load_inpschema(schema_version, create=False)
+
+    assert created_version == schema_version
+    assert dict_created == dict_stored
+
+@pytest.mark.parametrize('schema_version,schema_path', zip(outschema_versions, outschema_paths))
+def test_outschema_dict(schema_version, schema_path):
+    """
+    Test the fleur_schema_parser_functions to make sure that they match the stored inputschema_dict
+    """
+    dict_created, created_version = create_outschema_dict(schema_path, save_to_file=False)
+    dict_stored = load_outschema(schema_version, create=False)
 
     assert created_version == schema_version
     assert dict_created == dict_stored
