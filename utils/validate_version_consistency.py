42ce4d95773c6b32583a60f5ad0a2346564f311a
janssenhenning <henning.janssen@gmx.net>
2020-12-15 15:45:05 +0100

    add pyproject.toml to validate_version_consistency

--- utils/validate_version_consistency.py
+++ utils/validate_version_consistency.py
@@ -19,16 +19,25 @@ with open(os.path.join(ROOT_DIR, 'masci_tools/__init__.py')) as f:
 
 # Get the setup.py version number
 with open(os.path.join(ROOT_DIR, 'setup.py')) as f:
-   MATCH_EXPR = "version[^'\"]+(['\"])([^'\"]+)"
-   VERSION_SETUP = re.search(MATCH_EXPR, f.read()).group(2).strip()  # type: ignore
+    MATCH_EXPR = "version[^'\"]+(['\"])([^'\"]+)"
+    VERSION_SETUP = re.search(MATCH_EXPR, f.read()).group(2).strip()  # type: ignore
+
+# Get the pyproject.toml version number
+with open(os.path.join(ROOT_DIR, 'pyproject.toml')) as f:
+    MATCH_EXPR = "version[^'\"]+(['\"])([^'\"]+)"
+    VERSION_PYPROJECT = re.search(MATCH_EXPR, f.read()).group(2).strip()  # type: ignore
 
 # Get the .bumpversion.cfg current_version number
 with open(os.path.join(ROOT_DIR, '.bumpversion.cfg')) as f:
-   MATCH_EXPR = r"current_version\s*=\s*([^\\S\r\n]+)"
-   VERSION_BUMPVERSION = re.search(MATCH_EXPR, f.read()).group(1).strip()  # type: ignore
+    MATCH_EXPR = r'current_version\s*=\s*([^\\S\r\n]+)'
+    VERSION_BUMPVERSION = re.search(MATCH_EXPR, f.read()).group(1).strip()  # type: ignore
 
 if VERSION_INIT != VERSION_SETUP:
     print("Version numbers don't match: init:'{}', setup:'{}' ".format(VERSION_INIT, VERSION_SETUP))
+    sys.exit(1)
+
+if VERSION_INIT != VERSION_PYPROJECT:
+    print("Version numbers don't match: init:'{}', pyproject:'{}' ".format(VERSION_INIT, VERSION_PYPROJECT))
     sys.exit(1)
 
 if VERSION_INIT != VERSION_BUMPVERSION:
