18ca5357875195aed3ad52df7dd23951f8114094
janssenhenning <henning.janssen@gmx.net>
2021-01-26 17:24:23 +0100

    pre-commit run

--- masci_tools/tests/test_plot_methods.py
+++ masci_tools/tests/test_plot_methods.py
@@ -1102,49 +1102,21 @@ class TestResiduenPlot(object):  #pylint: disable=missing-class-docstring
         # need to return the figure in order for mpl checks to work
         return gcf()
 
+
 class TestPlotConvergenceResults(object):
 
-    energies = [-69269.46134019217,
-            -69269.42108466873,
-            -69269.35509388152,
-            -69269.62486438647,
-            -69269.51102655893,
-            -69269.48862754989,
-            -69269.48874847183,
-            -69269.48459145911,
-            -69269.47327003669,
-            -69269.47248623992,
-            -69269.47244891679,
-            -69269.47645687914,
-            -69269.47922946361,
-            -69269.4793222245,
-            -69269.47901836311,
-            -69269.47895198638,
-            -69269.47886053707,
-            -69269.47875692157,
-            -69269.47890881824,
-            -69269.47887586526]
+    energies = [
+        -69269.46134019217, -69269.42108466873, -69269.35509388152, -69269.62486438647, -69269.51102655893,
+        -69269.48862754989, -69269.48874847183, -69269.48459145911, -69269.47327003669, -69269.47248623992,
+        -69269.47244891679, -69269.47645687914, -69269.47922946361, -69269.4793222245, -69269.47901836311,
+        -69269.47895198638, -69269.47886053707, -69269.47875692157, -69269.47890881824, -69269.47887586526
+    ]
 
-    distances = [11.6508412231,
-                  10.5637525546,
-                  7.1938351319,
-                  2.6117836621,
-                  2.4735288205,
-                  2.9455389405,
-                  1.8364080301,
-                  1.4740568937,
-                  1.8542068593,
-                  0.9186745766,
-                  0.900191025,
-                  0.5290019787,
-                  0.0979035892,
-                  0.1098240811,
-                  0.0717916768,
-                  0.0258508395,
-                  0.0300810883,
-                  0.0067904499,
-                  0.0085097364,
-                  0.0073435947]
+    distances = [
+        11.6508412231, 10.5637525546, 7.1938351319, 2.6117836621, 2.4735288205, 2.9455389405, 1.8364080301,
+        1.4740568937, 1.8542068593, 0.9186745766, 0.900191025, 0.5290019787, 0.0979035892, 0.1098240811, 0.0717916768,
+        0.0258508395, 0.0300810883, 0.0067904499, 0.0085097364, 0.0073435947
+    ]
 
     iteration = range(len(distances))
 
@@ -1158,14 +1130,15 @@ class TestPlotConvergenceResults(object):
         gcf().clear()
 
         #plot_convergence produces two figures, for testing we merge them into one
-        fig, (ax1, ax2) = plt.subplots(1,2,figsize=(16,6))
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
 
-        plot_convergence_results(self.distances, self.energies, self.iteration, show=False,axis1=ax1, axis2=ax2)
+        plot_convergence_results(self.distances, self.energies, self.iteration, show=False, axis1=ax1, axis2=ax2)
 
         # need to return the figure in order for mpl checks to work
         return fig
 
-    @pytest.mark.mpl_image_compare(baseline_dir='files/plot_methods/matplotlib/convergence/', filename='param_change.png')
+    @pytest.mark.mpl_image_compare(baseline_dir='files/plot_methods/matplotlib/convergence/',
+                                   filename='param_change.png')
     def test_param_change(self):
         """
         Test of residuen plot with changed parameters
@@ -1175,9 +1148,19 @@ class TestPlotConvergenceResults(object):
         gcf().clear()
 
         #plot_convergence produces two figures, for testing we merge them into one
-        fig, (ax1, ax2) = plt.subplots(1,2,figsize=(16,6))
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
 
-        plot_convergence_results(self.distances, self.energies, self.iteration, show=False,axis1=ax1, axis2=ax2, linestyle='--', color='darkred', marker='s', linewidth=10, title_fontsize=20)
+        plot_convergence_results(self.distances,
+                                 self.energies,
+                                 self.iteration,
+                                 show=False,
+                                 axis1=ax1,
+                                 axis2=ax2,
+                                 linestyle='--',
+                                 color='darkred',
+                                 marker='s',
+                                 linewidth=10,
+                                 title_fontsize=20)
 
         # need to return the figure in order for mpl checks to work
         return fig
