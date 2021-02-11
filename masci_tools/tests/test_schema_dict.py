bbfe9f6ef2214de96bc69ffd18bd0b37a9da0c89
janssenhenning <henning.janssen@gmx.net>
2021-02-11 11:06:09 +0100

    Test the types of the keys in the schema dicts in separate tests

--- masci_tools/tests/test_schema_dict.py
+++ masci_tools/tests/test_schema_dict.py
@@ -18,6 +18,79 @@ for root, dirs, files in os.walk(os.path.abspath(os.path.join(CURRENT_DIR, SCHEM
                 schema_versions['out'].append(folder)
 
 
+def test_inpschema_dict_structure():
+    """
+    Test the types of the keys in the inpschemadict
+    """
+    from masci_tools.io.parsers.fleur.fleur_schema import InputSchemaDict
+    from masci_tools.util.case_insensitive_dict import CaseInsensitiveDict
+    from masci_tools.util.lockable_containers import LockableDict, LockableList
+
+    inputschema = InputSchemaDict.fromVersion('0.34')
+
+    EXPECTED_TYPES = {
+        'tag_paths': CaseInsensitiveDict,
+        'unique_attribs': CaseInsensitiveDict,
+        'unique_path_attribs': CaseInsensitiveDict,
+        'other_attribs': CaseInsensitiveDict,
+        'attrib_types': CaseInsensitiveDict,
+        'simple_elements': CaseInsensitiveDict,
+        '_basic_types': LockableDict,
+        'root_tag': str,
+        'inp_version': str,
+        'omitt_contained_tags': LockableList,
+        'tag_info': LockableDict
+    }
+
+    for key, val in inputschema.items():
+        assert isinstance(val, EXPECTED_TYPES[key])
+        if EXPECTED_TYPES[key] != str:
+            assert val._locked
+            if EXPECTED_TYPES[key] != CaseInsensitiveDict:
+                assert not isinstance(val,
+                                      CaseInsensitiveDict)  #since CaseInsensitiveDict is a subclass of LockableDict
+
+
+def test_outschema_dict_structure():
+    """
+    Test the types of the keys in the inpschemadict
+    """
+    from masci_tools.io.parsers.fleur.fleur_schema import OutputSchemaDict
+    from masci_tools.util.case_insensitive_dict import CaseInsensitiveDict
+    from masci_tools.util.lockable_containers import LockableDict, LockableList
+
+    outputschema = OutputSchemaDict.fromVersion('0.34')
+
+    EXPECTED_TYPES = {
+        'tag_paths': CaseInsensitiveDict,
+        'unique_attribs': CaseInsensitiveDict,
+        'unique_path_attribs': CaseInsensitiveDict,
+        'other_attribs': CaseInsensitiveDict,
+        'iteration_tag_paths': CaseInsensitiveDict,
+        'iteration_unique_attribs': CaseInsensitiveDict,
+        'iteration_unique_path_attribs': CaseInsensitiveDict,
+        'iteration_other_attribs': CaseInsensitiveDict,
+        'attrib_types': CaseInsensitiveDict,
+        'simple_elements': CaseInsensitiveDict,
+        '_basic_types': LockableDict,
+        '_input_basic_types': LockableDict,
+        'root_tag': str,
+        'input_tag': str,
+        'out_version': str,
+        'omitt_contained_tags': LockableList,
+        'tag_info': LockableDict,
+        'iteration_tag_info': LockableDict
+    }
+
+    for key, val in outputschema.items():
+        assert isinstance(val, EXPECTED_TYPES[key])
+        if EXPECTED_TYPES[key] != str:
+            assert val._locked
+            if EXPECTED_TYPES[key] != CaseInsensitiveDict:
+                assert not isinstance(val,
+                                      CaseInsensitiveDict)  #since CaseInsensitiveDict is a subclass of LockableDict
+
+
 @pytest.mark.parametrize('schema_version', schema_versions['inp'])
 def test_inpschema_dict(data_regression, schema_version):
     """
@@ -44,6 +117,10 @@ def test_outschema_dict(data_regression, inp_version, out_version):
 
 
 def clean_for_reg_dump(dict_to_clean):
+    """
+    Clean for data regression converts CaseInsensitiveFrozenSet to set
+    Lockable containers are to be converted berfore via `get_unlocked()`
+    """
     from masci_tools.util.case_insensitive_dict import CaseInsensitiveFrozenSet
 
     for key, val in dict_to_clean.items():
