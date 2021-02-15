9850a7d81e23c897777aff31ae28b59aacbb358a
janssenhenning <henning.janssen@gmx.net>
2021-02-15 19:16:44 +0100

    Fix bug for single lda+u procedure This was introduced, when evaluate_parent_tag was adjusted to the same rules as all other schem_dict_util functions for automatic list conversions For all other functions this is implicit since direct xpath evaluations for the attributes are done which will result in single strings

--- masci_tools/io/parsers/fleur/outxml_conversions.py
+++ masci_tools/io/parsers/fleur/outxml_conversions.py
@@ -142,6 +142,12 @@ def convert_ldau_definitions(out_dict, parser_info_out=None):
     parsed_ldau = out_dict['ldau_info'].pop('parsed_ldau')
     ldau_species = out_dict['ldau_info'].pop('ldau_species')
 
+    if isinstance(ldau_species['name'], str):
+        ldau_species = {key: [val] for key, val in ldau_species.items()}
+
+    if isinstance(parsed_ldau['l'], int):
+        parsed_ldau = {key: [val] for key, val in parsed_ldau.items()}
+
     ldau_definitions = zip(ldau_species['name'], ldau_species['atomic_number'], parsed_ldau['l'])
     for index, ldau_def in enumerate(ldau_definitions):
 
