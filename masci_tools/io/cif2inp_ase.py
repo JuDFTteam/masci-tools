31cc3b81553db850c3ee1b62055423594761979d
Matthias Redies <matthias.redies@posteo.de>
2021-02-24 18:25:27 +0100

    Update cif2inp_ase.py
    increase accuracy for output

--- masci_tools/io/cif2inp_ase.py
+++ masci_tools/io/cif2inp_ase.py
@@ -16,11 +16,11 @@ with open(inpFilename,"w+") as f:
     f.write(structureFormula+"\r\n")
     f.write("&input film=F /\r\n")
     for i in range(3):
-        f.write(' '.join(map("{:.4f}".format,structure.cell[i]))+"\r\n")
+        f.write(' '.join(map("{:.12f}".format,structure.cell[i]))+"\r\n")
     f.write("1.8897    !lattice const scaled as(1.0*bohr)\r\n1.0000 1.0000 1.0000    !scaling\r\n\r\n")
     f.write(str(natoms)+"\r\n")
     for i in range(natoms):
         f.write(str(structure.arrays['numbers'][i])+" "
-                +" ".join(map("{:.6f}".format, frac_coordinates[i]))+"\r\n")
+                +" ".join(map("{:.12f}".format, frac_coordinates[i]))+"\r\n")
     f.write("\r\n")
         
