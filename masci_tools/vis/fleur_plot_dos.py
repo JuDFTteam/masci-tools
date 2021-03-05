b934b1c371f3918698c43cf5603f307b151edf23
janssenhenning <henning.janssen@gmx.net>
2021-03-05 18:06:41 +0100

    Move selection into different function to allow selection for different types of dos to plot

--- masci_tools/vis/fleur_plot_dos.py
+++ masci_tools/vis/fleur_plot_dos.py
@@ -19,10 +19,6 @@ import warnings
 def fleur_plot_dos(path_to_dosfile,
                    path_to_dosfile_dn=None,
                    hdf_group='Local',
-                   interstitial=True,
-                   atoms='all',
-                   atoms_lresolved=None,
-                   atoms_area=False,
                    spinpol=True,
                    **kwargs):
     """
@@ -51,8 +47,16 @@ def fleur_plot_dos(path_to_dosfile,
         energy_grid = dos_data.pop('energyGrid') * HTR_TO_EV
 
         #Compute atom sums
-        for atom in range(1, natoms + 1):
-            dos_data[f'MT:{atom}'] = sum(value for key, value in dos_data.items() if f'MT:{atom}' in key)
+        if hdf_group == 'Local':
+            for atom in range(1, natoms + 1):
+                dos_data[f'MT:{atom}'] = sum(value for key, value in dos_data.items() if f'MT:{atom}' in key)
+        elif hdf_group == 'jDOS':
+            for atom in range(1, natoms + 1):
+                dos_data[f'MT:{atom}'] = sum(value for key, value in dos_data.items() if f'jDOS:{atom}' in key)
+        elif hdf_group == 'Orbcomp':
+            for atom in range(1, natoms + 1):
+                dos_data[f'MT:{atom}'] = sum(value for key, value in dos_data.items() if f'Orbcomp:{atom}' in key)
+
 
         if not spinpol:
             dos_data_up = {key: data[0, ...] / HTR_TO_EV for key, data in dos_data.items()}
@@ -63,6 +67,23 @@ def fleur_plot_dos(path_to_dosfile,
     else:
         #TODO: txt input
         raise NotImplementedError
+
+    if hdf_group == 'Local':
+        interstitial = kwargs.pop('interstitial',True)
+        atoms = kwargs.pop('interstitial','all')
+        l_resolved = kwargs.pop('l_resolved',None)
+        dos_data_up, dos_data_dn, keys_to_plot = select_from_Local(dos_data_up, dos_data_dn, natoms, interstitial, atoms, l_resolved)
+    else:
+        keys_to_plot = list(dos_data_up.keys())
+
+
+    if spinpol:
+        plot_spinpol_dos(dos_data_up, dos_data_dn, energy_grid, plot_label=keys_to_plot, **kwargs)
+    else:
+        plot_dos(dos_data_up, energy_grid, plot_label=keys_to_plot, **kwargs)
+
+
+def select_from_Local(dos_data_up, dos_data_dn, natoms, interstitial, atoms, l_resolved):
 
     keys_to_plot = {'Total'}
 
@@ -78,21 +99,18 @@ def fleur_plot_dos(path_to_dosfile,
     if atoms is not None:
         keys_to_plot.update(f'MT:{atom}' for atom in atoms)
 
-    if atoms_lresolved == 'all':
-        atoms_lresolved = range(1, natoms + 1)
-    elif atoms_lresolved is not None:
-        if not isinstance(atoms_lresolved, list):
-            atoms_lresolved = [atoms_lresolved]
+    if l_resolved == 'all':
+        l_resolved = range(1, natoms + 1)
+    elif l_resolved is not None:
+        if not isinstance(l_resolved, list):
+            l_resolved = [l_resolved]
 
-    if atoms_lresolved is not None:
-        keys_to_plot.update(f'MT:{atom}{orbital}' for atom in atoms_lresolved for orbital in 'spdf')
+    if l_resolved is not None:
+        keys_to_plot.update(f'MT:{atom}{orbital}' for atom in l_resolved for orbital in 'spdf')
 
     keys_to_plot = sorted(keys_to_plot)
     dos_data_up = [dos_data_up[key] for key in keys_to_plot]
     if dos_data_dn is not None:
         dos_data_dn = [dos_data_dn[key] for key in keys_to_plot]
 
-    if spinpol:
-        plot_spinpol_dos(dos_data_up, dos_data_dn, energy_grid, plot_label=keys_to_plot, **kwargs)
-    else:
-        plot_dos(dos_data_up, energy_grid, plot_label=keys_to_plot, **kwargs)
+    return dos_data_up, dos_data_dn, keys_to_plot
