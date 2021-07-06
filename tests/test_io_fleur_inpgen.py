d2fc61089b6fda2bc49f48c4ca8b65e9ad8f1446
janssenhenning <henning.janssen@gmx.net>
2021-07-06 16:21:57 +0200

    Add test for film input

--- tests/test_io_fleur_inpgen.py
+++ tests/test_io_fleur_inpgen.py
@@ -92,3 +92,30 @@ def test_write_inpgen_file_soc_qss(file_regression):
 
     file_regression.check(content)
 
+def test_write_inpgen_file_film(file_regression):
+
+    from masci_tools.io.io_fleur_inpgen import write_inpgen_file
+
+    cell = [[3.3168796764431, 0.0, 0.0], [1.6584398382215, 2.3453881115923, 0.0],
+                        [0.0, 0.0, 13.349076054836]]
+    kinds = [{'symbols': ('Fe',), 'weights': (1.0,), 'mass': 55.845, 'name': 'Fe'},
+             {'symbols': ('Nb',), 'weights': (1.0,), 'mass': 92.90638, 'name': 'Nb'}]
+    sites = [{'position': (1.6584398382215, 0.0, 8.2088583904803), 'kind_name': 'Fe'},
+             {'position': (0.0, 0.0, 10.096376717551), 'kind_name': 'Nb'},
+             {'position': (1.6584398382215, 0.0, 12.46832205832), 'kind_name': 'Nb'}]
+
+    parameters = {
+        'comp': {
+            'kmax': 5.0,
+            'gmaxxc': 12.5,
+            'gmax': 15.0
+        },
+    }
+
+    with tempfile.NamedTemporaryFile('r') as tmp:
+
+        write_inpgen_file(cell, sites, kinds, input_params=parameters, path = tmp.name, pbc= (True,True,False))
+        content = tmp.read()
+
+    file_regression.check(content)
+
