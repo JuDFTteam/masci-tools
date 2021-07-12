247b19cb27254ea522bd3ddd31603c7685807ec8
janssenhenning <henning.janssen@gmx.net>
2021-07-12 12:46:14 +0200

    Fix for ``legend_show_data_labels`` option with new list_of_dicts switch

--- masci_tools/vis/matplotlib_plotter.py
+++ masci_tools/vis/matplotlib_plotter.py
@@ -433,14 +433,14 @@ class MatplotlibPlotter(Plotter):
                 plot_kwargs.pop('area_linecolor', None)
 
         if self['legend_show_data_labels']:
-            if not self.single_plot:
+            if not self.single_plot and list_of_dicts:
                 for index, value in enumerate(plot_kwargs):
                     if value.get('label') is None:
                         value.pop('label', None)
             elif plot_kwargs.get('label') is None:
                 plot_kwargs.pop('label', None)
         else:
-            if not self.single_plot:
+            if not self.single_plot and list_of_dicts:
                 for index, value in enumerate(plot_kwargs):
                     if 'label' not in value:
                         value['label'] = None
