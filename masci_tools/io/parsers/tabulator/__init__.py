679639e3c3d2b37536127b206d1ed80e4f31ad11
johannes wasmer <johannes.wasmer@gmail.com>
2021-10-05 23:34:53 +0200

    io: added parser 'tabulator' (objects->pandas DataFrame)

---
+++ masci_tools/io/parsers/tabulator/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Copyright (c), Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany.         #
+#                All rights reserved.                                         #
+# This file is part of the Masci-tools package.                               #
+# (Material science tools)                                                    #
+#                                                                             #
+# The code is hosted on GitHub at https://github.com/judftteam/masci-tools.   #
+# For further information on the license, see the LICENSE.txt file.           #
+# For further information please visit http://judft.de/.                      #
+#                                                                             #
+###############################################################################
+"""This subpackage contains a tabulator. Its purpose is to let you create a table of properties,
+say, a pandas DataFrame, from any collections of similar objects, and reused frequently used recipes.
+"""
+# import submodules
+from . import transformers
+from . import recipes
+from . import tabulator
---
+++ masci_tools/io/parsers/tabulator/transforms.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Copyright (c), Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany.         #
+#                All rights reserved.                                         #
+# This file is part of the Masci-tools package.                               #
+# (Material science tools)                                                    #
+#                                                                             #
+# The code is hosted on GitHub at https://github.com/judftteam/masci-tools.   #
+# For further information on the license, see the LICENSE.txt file.           #
+# For further information please visit http://judft.de/.                      #
+#                                                                             #
+###############################################################################
+"""This subpackage contains transformers for the tabulator subpackage, which turns
+properties of a collections of objects into a table.
+
+Transformers let you transform properties while they get tabulated.
+"""
