627c7fbbaa09683fffe8e1d274767934fd37fdd3
janssenhenning <henning.janssen@gmx.net>
2021-11-22 15:01:45 +0100

    Refactor

--- masci_tools/util/xml/xpathbuilder.py
+++ masci_tools/util/xml/xpathbuilder.py
@@ -41,6 +41,7 @@ class XPathBuilder:
         self.path_kwargs = kwargs
         self.filters: Dict[str, FilterType] = {}
         self.path_variables: Dict[str, 'etree._XPathObject'] = {}
+        self.value_conditions: int = 0
         if filters is not None:
             for key, val in filters.items():
                 self.add_filter(key, val)
@@ -70,74 +71,85 @@ class XPathBuilder:
 
         return self.components.pop(-1)
 
+    def get_predicate(self, tag, condition):
+
+        if not isinstance(condition, dict):
+            condition = {'=': condition}
+
+        if len(condition) > 1:
+            raise ValueError('Only one condition allowed in dict')
+
+        operator, content = dict(condition).popitem()
+        if operator == '==':
+            operator = '='
+
+        return self.process_condition(tag, operator, content)
+
+    def process_condition(self, tag, operator, content):
+
+        if operator == 'and':
+            if not isinstance(content, (list, tuple)):
+                raise TypeError('For and operator and provide the conditions as a list')
+            predicates = [self.get_predicate(tag, condition_part) for condition_part in content]
+            predicate = ' and '.join(predicates)
+        elif operator == 'or':
+            if not isinstance(content, (list, tuple)):
+                raise TypeError('For or operator and provide the conditions as a list')
+            predicates = [self.get_predicate(tag, condition_part) for condition_part in content]
+            predicate = ' or '.join(predicates)
+        elif operator == 'has':
+            predicate = f'${tag}_has'
+            self.path_variables[f'{tag}_has'] = content
+        elif operator == 'has-not':
+            predicate = f'${tag}_has_not'
+            self.path_variables[f'{tag}_has_not'] = content
+        elif operator == 'index':
+            if isinstance(content, int):
+                if content == -1:
+                    predicate = 'last()'
+                elif content == 1:
+                    predicate = 'first()'
+                elif content < 0:
+                    predicate = f'last() - ${tag}_index'
+                    self.path_variables[f'{tag}_index'] = content + 1
+                else:
+                    predicate = f'${tag}_index'
+                    self.path_variables[f'{tag}_index'] = content
+            else:
+                cond, index = dict(content).popitem()
+                predicate = f'position() {cond} ${tag}_index'
+                self.path_variables[f'{tag}_index'] = index
+        elif '/' not in tag:
+            cond, value = dict(content).popitem()
+
+            variable_name = f'${tag}_cond_{self.value_conditions}_name'
+            value_variable_name = f'${tag}_cond_{self.value_conditions}'
+
+            if cond == 'contains':
+                predicate = f'contains(@*[local-name()={variable_name}],{value_variable_name})'
+            elif cond == 'not-contains':
+                predicate = f'not contains(@*[local-name()={variable_name}],{value_variable_name})'
+            else:
+                predicate = f'@*[local-name()={variable_name}] {cond} {value_variable_name}'
+
+            self.path_variables[variable_name] = operator
+            self.path_variables[value_variable_name] = value
+            self.value_conditions += 1
+        else:
+            raise NotImplementedError('Conditions based on subtags not implemented')
+
+        return predicate
+
     @property
     def path(self) -> 'etree._xpath':
 
         predicates = [''] * len(self.components)
         self.path_variables = {}
+        self.value_conditions = 0
 
-        for tag, conditions in self.filters.items():
-
+        for tag, condition in self.filters.items():
             component_index = self.components.index(tag)
-            predicate = ''
-
-            value_conditions = 0
-            for condition_name, condition in conditions.items():
-
-                if condition_name == 'has':
-                    if predicate:
-                        predicate = f'{predicate} and ${tag}_has'
-                    else:
-                        predicate = f'${tag}_has'
-                    self.path_variables[f'{tag}_has'] = condition
-                if condition_name == 'has-not':
-                    if predicate:
-                        predicate = f'{predicate} and ${tag}_has_not'
-                    else:
-                        predicate = f'${tag}_has_not'
-                    self.path_variables[f'{tag}_has'] = condition
-                elif condition_name == 'index':
-                    if isinstance(condition, int):
-                        if condition == -1:
-                            index_condition = 'last()'
-                        elif condition == 1:
-                            index_condition = 'first()'
-                        elif condition < 0:
-                            index_condition = f'last() - ${tag}_index'
-                            self.path_variables[f'{tag}_index'] = condition + 1
-                        else:
-                            index_condition = f'${tag}_index'
-                            self.path_variables[f'{tag}_index'] = condition
-                    else:
-                        cond, index = dict(condition).popitem()
-                        index_condition = f'position() {cond} ${tag}_index'
-                        self.path_variables[f'{tag}_index'] = index
-
-                    if predicate:
-                        predicate = f'{predicate} and {index_condition}'
-                    else:
-                        predicate = index_condition
-                elif '/' not in condition_name:
-                    cond, value = dict(condition).popitem()
-                    if cond == 'contains':
-                        value_condition = f'contains(@*[local-name()=${tag}_cond_{value_conditions}_name],${tag}_cond_{value_conditions})'
-                    elif cond == 'not-contains':
-                        value_condition = f'not contains(@*[local-name()=${tag}_cond_{value_conditions}_name],${tag}_cond_{value_conditions})'
-                    else:
-                        value_condition = f'@*[local-name()=${tag}_cond_{value_conditions}_name] {cond} ${tag}_cond_{value_conditions}'
-
-                    self.path_variables[f'{tag}_cond_{value_conditions}_name'] = condition_name
-                    self.path_variables[f'{tag}_cond_{value_conditions}'] = value
-                    value_conditions += 1
-
-                    if predicate:
-                        predicate = f'{predicate} and {value_condition}'
-                    else:
-                        predicate = value_condition
-                else:
-                    raise NotImplementedError('Conditions based on subtags not implemented')
-
-            predicates[component_index] = predicate
+            predicates[component_index] = self.get_predicate(tag, condition)
 
         path = '/'.join(
             [f'{tag}[{predicate}]' if predicate else tag for tag, predicate in zip(self.components, predicates)])
