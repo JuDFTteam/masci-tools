98f4910e0018f6d95564cb719d6483a47c786c25
janssenhenning <henning.janssen@gmx.net>
2022-01-12 19:13:44 +0100

    Pin sphinx-toolbox to fix docs failure

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
             'pre-commit':
             ['mypy==0.930', 'pre-commit>=2.6.0', 'yapf>=0.30.0', 'pylint~=2.11.1', 'pytest~=6.0', 'lxml-stubs'],
             'docs':
-            ['Sphinx', 'docutils', 'sphinx_rtd_theme', 'sphinx-click', 'sphinx-toolbox', 'sphinx-autodoc-typehints'],
+            ['Sphinx', 'docutils', 'sphinx_rtd_theme', 'sphinx-click', 'sphinx-toolbox<=2.15.2', 'sphinx-autodoc-typehints'],
             'testing': ['pytest~=6.0', 'pytest-cov', 'pytest-mpl>=0.12', 'pytest-regressions>=1.0'],
             'bokeh-plots': [
                 'bokeh<=1.4.0'  # versions beyond 1.4.0 require a tornardo version not compatible with aiida-core /circus
