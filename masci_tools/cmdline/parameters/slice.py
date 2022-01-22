9122de9633f967b1b171494948e4547d9bcc1c78
janssenhenning <henning.janssen@gmx.net>
2022-01-22 14:40:09 +0100

    Typing click parameters and adjust test

--- masci_tools/cmdline/parameters/slice.py
+++ masci_tools/cmdline/parameters/slice.py
@@ -1,8 +1,15 @@
 """
 Click parameters for easily selecting multiple elements from a list via indices
 """
-from typing import Sequence, Any
+from __future__ import annotations
+
+from typing import Generic, Sequence, TypeVar, Any
 import click
+
+T = TypeVar('T')
+"""
+Generic Type variable
+"""
 
 
 class IntegerSlice(click.ParamType):
@@ -11,7 +18,7 @@ class IntegerSlice(click.ParamType):
     """
     name = 'integer-range'
 
-    def convert(self, value, param, ctx):
+    def convert(self, value: Any, param: click.Parameter | None, ctx: click.Context | None) -> int | slice:
         if isinstance(value, int):
             return value
 
@@ -25,23 +32,24 @@ class IntegerSlice(click.ParamType):
                 'Please provide either an integer number or two integer numbers separated by -') from exc
 
 
-class ListElement(IntegerSlice):
+class ListElement(IntegerSlice, Generic[T]):
     """
     Click parameter for choosing an (or multiple) element(s) from a list
     """
 
-    def __init__(self, data: Sequence[Any], return_list: bool=False) -> None:
+    def __init__(self, data: Sequence[T], return_list: bool = False) -> None:
         self.data = data
         self.return_list = return_list
         super().__init__()
 
-    def convert(self, value, param, ctx):
+    def convert(  #type:ignore[override]
+            self, value: Any, param: click.Parameter | None, ctx: click.Context | None) -> T | Sequence[T]:
         indices = super().convert(value, param, ctx)
 
         try:
             values = self.data[indices]
             if not isinstance(values, list) and self.return_list:
-                values = [values]
+                values = [values]  #type:ignore
         except IndexError as exc:
             raise click.BadParameter(
                 f'Please provide indices in the range of a list with length {len(self.data)}') from exc
