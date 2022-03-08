672ceefc524b72661720e25e28dff36a32079f82
janssenhenning <henning.janssen@gmx.net>
2022-03-08 11:46:50 +0100

    Add tests for last commit

--- tests/vis/test_fleur_vis.py
+++ tests/vis/test_fleur_vis.py
@@ -548,6 +548,39 @@ def test_plot_dos_selection_bokeh(check_bokeh_plot):
     check_bokeh_plot(fig)
 
 
+@pytest.mark.mpl_image_compare(baseline_dir=MPL_BASELINE_DIR, filename='spinpol_dos_non_spinpol.png')
+def test_plot_spinpol_dos_non_spinpol_mpl():
+    from masci_tools.io.parsers.hdf5 import HDF5Reader
+    from masci_tools.io.parsers.hdf5.recipes import FleurDOS
+    from masci_tools.vis.fleur import plot_fleur_dos
+
+    TEST_BANDDOS_FILE = os.path.join(HDFTEST_DIR, 'banddos_spinpol_dos.hdf')
+
+    with HDF5Reader(TEST_BANDDOS_FILE) as h5reader:
+        data, attributes = h5reader.read(recipe=FleurDOS)
+
+    gcf().clear()
+
+    plot_fleur_dos(data, attributes, show=False, spinpol=False)
+
+    return gcf()
+
+
+def test_plot_spinpol_dos_non_spinpol_bokeh(check_bokeh_plot):
+    from masci_tools.io.parsers.hdf5 import HDF5Reader
+    from masci_tools.io.parsers.hdf5.recipes import FleurDOS
+    from masci_tools.vis.fleur import plot_fleur_dos
+
+    TEST_BANDDOS_FILE = os.path.join(HDFTEST_DIR, 'banddos_spinpol_dos.hdf')
+
+    with HDF5Reader(TEST_BANDDOS_FILE) as h5reader:
+        data, attributes = h5reader.read(recipe=FleurDOS)
+
+    fig = plot_fleur_dos(data, attributes, show=False, backend='bokeh', spinpol=False)
+
+    check_bokeh_plot(fig)
+
+
 @pytest.mark.mpl_image_compare(baseline_dir=MPL_BASELINE_DIR, filename='bands_character.png')
 def test_plot_bands_characterize_mpl():
     from masci_tools.io.parsers.hdf5 import HDF5Reader
