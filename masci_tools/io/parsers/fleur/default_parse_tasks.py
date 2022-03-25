8d7151bede34c8845dc65fbb4a29205193967ccf
janssenhenning <henning.janssen@gmx.net>
2022-03-25 11:45:24 +0100

    formatting

--- masci_tools/io/parsers/fleur/default_parse_tasks.py
+++ masci_tools/io/parsers/fleur/default_parse_tasks.py
@@ -313,16 +313,18 @@ TASKS_DEFINITION = {
     'total_energy': {
         '_minimal':
         True,
-        '_conversions':
-        [Conversion(name='convert_htr_to_ev', kwargs={
-            'name': 'energy_hartree',
-            'converted_name': 'energy',
-        }),
-        Conversion(name='convert_htr_to_ev', kwargs={
-            'name': 'ts_energy_hartree',
-            'converted_name': 'ts_energy',
-            'pop': True
-        })],
+        '_conversions': [
+            Conversion(name='convert_htr_to_ev', kwargs={
+                'name': 'energy_hartree',
+                'converted_name': 'energy',
+            }),
+            Conversion(name='convert_htr_to_ev',
+                       kwargs={
+                           'name': 'ts_energy_hartree',
+                           'converted_name': 'ts_energy',
+                           'pop': True
+                       })
+        ],
         'energy_hartree': {
             'parse_type': 'attrib',
             'path_spec': {
