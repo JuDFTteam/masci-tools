9d19581a4412ee4408059d3c1aee5e4fa39d9bc2
janssenhenning <henning.janssen@gmx.net>
2022-06-12 17:08:28 +0200

    Improve diff function
    - More consistent style options (less hardcoding)
    - typing fixes
    - formatting

--- masci_tools/util/ipython.py
+++ masci_tools/util/ipython.py
@@ -13,6 +13,7 @@ from typing import Any
 import difflib
 from IPython.display import HTML
 
+
 def display_xml(data: XMLLike) -> str:
     """
     Display the given lxml XML tree as formatted HTML
@@ -23,9 +24,29 @@ def display_xml(data: XMLLike) -> str:
     """
 
     xmlstring = etree.tostring(data, encoding='unicode', pretty_print=True)
-    return highlight(xmlstring, XmlLexer(), HtmlFormatter(noclasses=True,nobackground=False, style='monokai'))
+    return highlight(xmlstring, XmlLexer(), HtmlFormatter(noclasses=True, nobackground=False))
 
-def xml_diff(old: XMLLike, new: XMLLike):
+
+def xml_diff(old: XMLLike, new: XMLLike, indent: bool = True) -> HTML:
+    """
+    Create a diff of two lxml trees with HTML with syntax highlighting
+
+    :param old: original XML tree
+    :param new: modified XML tree
+    :param indent: bool, if True etree.indent is called on the trees before diff
+    """
+
+    STYLES = {
+        'control': 'font-weight: bold',
+        'delete': 'background-color: hsla(0, 100%, 74%, 0.5); color: #000000;',
+        'delete-detail': 'background-color: hsla(0, 100%, 74%, 0.5); color: #000000;',
+        'insert': 'background-color: hsla(102, 100%, 74%, 0.5); color: #000000;',
+        'insert-detail': 'background-color: hsla(102, 100%, 74%, 0.5); color: #000000;',
+    }
+
+    if indent:
+        etree.indent(old)
+        etree.indent(new)
 
     old_lines = etree.tostring(old, encoding='unicode', pretty_print=True).split('\n')
     new_lines = etree.tostring(new, encoding='unicode', pretty_print=True).split('\n')
@@ -40,34 +61,8 @@ def xml_diff(old: XMLLike, new: XMLLike):
     lines = lines[first_block:]
     lines.reverse()
 
-    diff_blocks = []
-    current_block = None
-
-    css = """\
-    <style type="text/css">
-        .diff .control {
-            background-color: #eaf2f5;
-            color: #999999;
-        }
-
-        .insert {
-            background-color: hsla(102, 100%, 74%, 0.5);
-            color: #000000;
-        }
-        .insert .detail {
-            background-color: hsla(102, 100%, 50%, 0.5);
-            color: #000000;
-        }
-        .delete {
-            background-color: hsla(0, 100%, 74%, 0.5); //semi-transparent red
-            color: #000000;
-        }
-        .delete .detail {
-            background-color:  hsla(0, 100%, 50%, 0.5);
-            color: #000000;
-        }
-    </style>
-    """
+    diff_blocks: list[str] = []
+    current_block: list[str] = []
 
     def highlight_xml(xmlstring):
         return highlight(xmlstring, XmlLexer(), HtmlFormatter(noclasses=True, nowrap=True)).rstrip('\n')
@@ -78,61 +73,61 @@ def xml_diff(old: XMLLike, new: XMLLike):
         aline = []
         bline = []
         for tag, i1, i2, j1, j2 in difflib.SequenceMatcher(a=a, b=b).get_opcodes():
-            if tag == "equal":
+            if tag == 'equal':
                 aline.append(a[i1:i2])
                 bline.append(b[j1:j2])
                 continue
-            aline.append(f'<span class="detail">{a[i1:i2]}</span>')
-            bline.append(f'<span class="detail">{b[j1:j2]}</span>')
-        return "".join(aline), "".join(bline)
+            aline.append(f'<span style="{STYLES["delete-detail"]}">{a[i1:i2]}</span>')
+            bline.append(f'<span style="{STYLES["insert-detail"]}">{b[j1:j2]}</span>')
+        return ''.join(aline), ''.join(bline)
 
     while lines:
         line = lines.pop()
         if line.startswith('@@') or not lines:
-            line = line.strip('\n@- ').replace(',',' (')
-            print(line)
+            line = line.strip('\n@- ').replace(',', ' (')
             original, _, changed = line.partition('+')
-            line = f'Original: line {original} lines)\n' \
-                f' Changed line {changed} lines)'
+            control_lines = [f'Original: line {original} lines)', f'Changed line {changed} lines)']
 
-            if current_block is None:
-                current_block = [f'<span class="control"> {line}</span>']
+            if len(current_block) == 0:
+                current_block = [f'<span style="{STYLES["control"]}"> {line}</span>' for line in control_lines]
             else:
                 current_block.append('<span></span>')
                 diff_blocks.append('\n'.join(current_block))
-                current_block = [f'<span class="control"> {line}</span>']
+                current_block = [f'<span style="{STYLES["control"]}"> {line}</span>' for line in control_lines]
         elif line.startswith('-'):
             if lines:
-                _next = []
+                _next: list[str] = []
                 while lines and len(_next) < 2:
                     _next.append(lines.pop())
-                if _next[0].startswith("+") and (
-                        len(_next) == 1 or _next[1][0] not in ("+", "-")):
+                if _next[0].startswith('+') and (len(_next) == 1 or _next[1][0] not in ('+', '-')):
                     aline, bline = _line_diff(line[1:], _next.pop(0)[1:])
-                    current_block.append(f'<span class="delete"> {aline}</span>')
-                    current_block.append(f'<span class="insert"> {bline}</span>')
+                    current_block.append(f'<span style="{STYLES["delete"]}"> {aline}</span>')
+                    current_block.append(f'<span style="{STYLES["insert"]}"> {bline}</span>')
                     if _next:
                         lines.append(_next.pop())
                     continue
                 lines.extend(reversed(_next))
-            current_block.append(f'<span class="delete"> {highlight_xml(line[1:])}</span>')
+            current_block.append(f'<span style="{STYLES["delete"]}"> {highlight_xml(line[1:])}</span>')
         elif line.startswith('+'):
-            current_block.append(f'<span class="insert"> {highlight_xml(line[1:])}</span>')
-        elif current_block is not None:
+            current_block.append(f'<span style="{STYLES["insert"]}"> {highlight_xml(line[1:])}</span>')
+        elif len(current_block) > 0:
             current_block.append(highlight_xml(line))
 
-    diff_blocks = '\n'.join(diff_blocks)
-    
-    return HTML(f"""
-    {css}
-    <pre style="line-height: 125%;"><span></span>
-    {diff_blocks}
-    </pre>
-    """ )
+    diff = '\n'.join(diff_blocks)
+
+    formatter = HtmlFormatter()
+
+    return HTML(f"""<div class="{formatter.cssclass}" style="background: {formatter.style.background_color}">
+<pre style="line-height: 125%;">
+{diff}
+</pre></div>
+""")
 
 
 def register_formatters(ipython: Any) -> None:
-
+    """
+    Register formatter of lxml trees in HTML form
+    """
     html_formatter = ipython.display_formatter.formatters['text/html']
     html_formatter.for_type(etree._Element, display_xml)
     html_formatter.for_type(etree._ElementTree, display_xml)
