7cb8338408a8f74eaec1a0b7594ed62a192aff55
janssenhenning <henning.janssen@gmx.net>
2022-12-07 14:24:22 +0100

    More tests

--- tests/cmdline/test_convert_inpgen.py
+++ tests/cmdline/test_convert_inpgen.py
@@ -1,7 +1,6 @@
 """
-Tests of the commands in the fleur-schema subgroup of the masci-tools cli
+Tests of the commands in the convert-inpgen command of the masci-tools cli
 """
-from pathlib import Path
 import os
 import pytest
 
@@ -18,16 +17,16 @@ except ImportError:
 
 
 @pytest.mark.skipif(not pymatgen, reason='pymatgen or pymatgen-io-fleur are not installed')
-def test_pymatgen_converter(file_regression):
+def test_pymatgen_converter(file_regression, test_file):
     """
     Test of the convert-inpgen command using the pymatgen converter
     """
     from masci_tools.cmdline.commands.convert_inpgen import convert_inpgen
     from click.testing import CliRunner
 
-    TEST_FILE = Path(__file__).parent.resolve() / Path('../files/fleur/Max-R5/SiLOXML/files/inp.xml')
+    TEST_FILE = test_file('fleur/Max-R5/SiLOXML/files/inp.xml')
     runner = CliRunner()
-    args = [os.fspath(TEST_FILE), 'inpgen.in', '--converter', 'pymatgen']
+    args = [TEST_FILE, 'inpgen.in', '--converter', 'pymatgen']
     with runner.isolated_filesystem():
         result = runner.invoke(convert_inpgen, args)
 
@@ -42,17 +41,42 @@ def test_pymatgen_converter(file_regression):
     file_regression.check(content, basename='test_convert_inpgen')
 
 
+@pytest.mark.skipif(not pymatgen, reason='pymatgen or pymatgen-io-fleur are not installed')
+def test_pymatgen_converter_cif(file_regression, test_file):
+    """
+    Test of the convert-inpgen command using the pymatgen converter
+    """
+    from masci_tools.cmdline.commands.convert_inpgen import convert_inpgen
+    from click.testing import CliRunner
+
+    TEST_FILE = test_file('cif_files/Si.cif')
+    runner = CliRunner()
+    args = [TEST_FILE, 'inpgen.in', '--converter', 'pymatgen']
+    with runner.isolated_filesystem():
+        result = runner.invoke(convert_inpgen, args)
+
+        print(result.output)
+        assert result.exception is None, f'An unexpected exception occurred: {result.exception}'
+
+        assert os.path.isfile('inpgen.in')
+
+        with open('inpgen.in', encoding='utf-8') as file:
+            content = file.read()
+
+    file_regression.check(content, basename='test_convert_inpgen_cif')
+
+
 @pytest.mark.skipif(not ase_fleur, reason='ase-fleur is not installed')
-def test_ase_converter(file_regression):
+def test_ase_converter(file_regression, test_file):
     """
     Test of the convert-inpgen command using the ase converter
     """
     from masci_tools.cmdline.commands.convert_inpgen import convert_inpgen
     from click.testing import CliRunner
 
-    TEST_FILE = Path(__file__).parent.resolve() / Path('../files/fleur/Max-R5/SiLOXML/files/inp.xml')
+    TEST_FILE = test_file('fleur/Max-R5/SiLOXML/files/inp.xml')
     runner = CliRunner()
-    args = [os.fspath(TEST_FILE), 'inpgen.in', '--converter', 'ase']
+    args = [TEST_FILE, 'inpgen.in', '--converter', 'ase']
     with runner.isolated_filesystem():
         result = runner.invoke(convert_inpgen, args)
 
@@ -64,4 +88,4 @@ def test_ase_converter(file_regression):
         with open('inpgen.in', encoding='utf-8') as file:
             content = file.read()
 
-    file_regression.check(content, basename='test_convert_inpgen')
+    file_regression.check(content, basename='test_convert_inpgen_ase')
