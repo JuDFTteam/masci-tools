44cffa5ba03705326381b01c646eb2da45b306ed
Robin Hilgers <87805664+RobinHilg@users.noreply.github.com>
2023-03-10 15:53:21 -0800

    vec_magn_moments

--- masci_tools/io/parsers/fleur/default_parse_tasks.py
+++ masci_tools/io/parsers/fleur/default_parse_tasks.py
@@ -502,6 +502,17 @@ TASKS_DEFINITION = {
             }
         }
     },
+    'vec_magn_moments': {
+        '_minimum_version': '0.36',
+        '_modes': [('jspins', 2)],
+        'vec_magn_moments': {
+            'parse_type': 'attrib',
+            'path_spec': {
+                'name': 'vec',
+                'tag_name': 'globalMagMoment'
+            }
+        }
+    },
     'forces': {
         '_minimal': True,
         '_modes': [('relax', True)],
